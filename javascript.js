5065bc76cfc7add5c04a3160df155488717ab843
MAMcC0 <86750930+MAMcC0@users.noreply.github.com>
2022-05-15 16:06:49 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+function growButton(){
+    document.getElementById("box").style = "height:500px; width:500px; background-color:orange; margin:25px";
+}
+
+function turnBlue(){
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function fadeButton(){
+    document.getElementById("box").style.opacity = "0.5";
+}
+
+function resetButton(){
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px";
+}
